3535a5630e1b8f81ae7e04cde33aabbdbd37a3a1
Naoufel <Naoufel@Naoufel-Pc>
2017-01-18 14:34:12 +0000

    First commit

---
+++ BanqueProject/src/main/java/org/opendevup/BanqueProjectApplication.java
@@ -0,0 +1,12 @@
+package org.opendevup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueProjectApplication.class, args);
+	}
+}
